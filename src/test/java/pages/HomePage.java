a5d954aa2949af33f24acaac3f8bcf79d8b6fbae
suraksha <surakshajiddewar21@gmail.com>
2021-03-30 22:39:27 +0530

    Updated Home page

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,6 +5,7 @@ public class HomePage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Home Page 1");
+		System.out.println("Home Page 2");
 
 	}
 
