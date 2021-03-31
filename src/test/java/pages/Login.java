3cff70e4c3e5b2d31ae59244c4fd9503ce77251b
suraksha <surakshajiddewar21@gmail.com>
2021-03-31 12:37:57 +0530

    Login 3

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -4,8 +4,8 @@ public class Login {
 
 	public static void main(String[] args) {
 		System.out.println("Login1");
-		System.out.println("Login1");
-
+		System.out.println("Login2");
+		System.out.println("Login3");
 	}
 
 }
