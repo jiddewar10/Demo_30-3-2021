5fbb508c3cbe57174c98aa14f10036185efd5e4f
suraksha <surakshajiddewar21@gmail.com>
2021-03-31 12:21:01 +0530

    Kogin 2

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -3,7 +3,8 @@ package pages;
 public class Login {
 
 	public static void main(String[] args) {
-		System.out.println("Login");
+		System.out.println("Login1");
+		System.out.println("Login1");
 
 	}
 
