f2e604f9c4d2d39e2d919c696f65f06cc075fd49
suraksha <surakshajiddewar21@gmail.com>
2021-03-31 12:18:43 +0530

    Home page 3

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,6 +6,7 @@ public class HomePage {
 		// TODO Auto-generated method stub
 		System.out.println("Home Page 1");
 		System.out.println("Home Page 2");
+		System.out.println("Home Page 3");
 
 	}
 
